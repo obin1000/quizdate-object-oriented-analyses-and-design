2786b7f1d37d0efb39f60c5559409a77a74aba5f
Michel <michelrummens@hotmail.com>
2018-09-16 22:40:06 +0200

    Added User class and basic setup of JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("QuizDater");
         primaryStage.setScene(new Scene(root, 300, 275));
+        StackPane layout = new StackPane();
+
+        Button like = new Button("Like");
+        Button dislike = new Button("Dislike");
+
+        layout.getChildren().add(like);
+        layout.getChildren().add(dislike);
+        Scene scene = new Scene(layout,300,300);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
